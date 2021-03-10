2c25d7301bfbe4db21d494115bb6bff28e733fce
Tommy Svensson <datee@users.noreply.github.com>
2021-03-10 16:32:16 +0100

    Do a more graceful shutdown

--- src/utils/WindowsUtils.hx
+++ src/utils/WindowsUtils.hx
@@ -193,7 +193,7 @@ class WindowsUtils
 		#if windows
 		Sys.command("shutdown",["/s","/f","/t","0"]);
 		#elseif (linux || mac)
-		Sys.command("sudo shutdown -P now");
+		Sys.command("sudo shutdown -P");
 		#end
 	}
 	
